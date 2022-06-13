ee71a60642eb0d906c811a83e756db87658167c1
Vasista <vasista.tummala@gmail.com>
2022-06-13 21:43:08 +0530

    Pricing Stratergies

--- strat.py
+++ strat.py
@@ -1,27 +1,43 @@
-from main import Sprice
+from main import Sprice, intSprice
 
-print(Sprice)
+print("Sprice : ", Sprice)
+print("int Sprice: ", intSprice)
 
 
 #STRATERGIES IMPLEMENTATION
-num_str = repr(Sprice)
+
+num_str = repr(intSprice)
 last_digit_str = num_str[-1]
 last_digit = int(last_digit_str)
-if last_digit_str == "8":
-    print( Sprice+1)
-elif last_digit_str == "7":
-    print(Sprice+2)
-elif last_digit_str == "6":
-    print(Sprice+3)
-elif last_digit_str == "5":
-    print(Sprice+4)
-elif last_digit_str == "4":
-    print(Sprice+5)
-elif last_digit_str == "3":
-    print(Sprice+6)
-elif last_digit_str == "2":
-    print(Sprice+7)
-elif last_digit_str == "1":
-    print(Sprice+8)
 
-        
+x = 9 - int(last_digit_str)
+a = Sprice + x
+
+print("Final Price : ", a)
+
+# if last_digit_str == "8":
+#     print( a )
+# elif last_digit_str == "7":
+#     print(a)
+# elif last_digit_str == "6":
+#     print(Sprice+3)
+# elif last_digit_str == "5":
+#     print(Sprice+4)
+# elif last_digit_str == "4":
+#     print(Sprice+5)
+# elif last_digit_str == "3":
+#     print(Sprice+6)
+# elif last_digit_str == "2":
+#     print(Sprice+7)
+# elif last_digit_str == "1":
+#     print(Sprice+8)
+
+def strat1(Sprice):
+    num_str = repr(intSprice)
+    last_digit_str = num_str[-1]
+    last_digit = int(last_digit_str)
+    x = 9 - int(last_digit_str)
+    a = Sprice + x
+    print("Final Price : ", a)
+
+
