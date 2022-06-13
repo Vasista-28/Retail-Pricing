3a61453d49ab795e6be6d0792c537d2fbd90915c
Vasista <vasista.tummala@gmail.com>
2022-06-13 21:14:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,3 @@
-# # import pandas as pd
-
-# # def retailPricing(imp, *args):
-# #     inv=100
-# #     sellthrough=20
-# #     cost=10
-# #     profitpercentage=2
-# #     perishability=0
-# #     OPEX=25
-# #     print(OPEX + cost)
-# #     print(inv+sellthrough+cost+profitpercentage+perishability+OPEX)
-# # retailPricing(inv,sellthrough,cost,profitpercentage,perishability,OPEX)
-
-
-
-# def fn(a):
-#     return 10 * a
-
-# lst = [1, 2, 3, 4, 5, 6]
-
-# for i in lst:
-#     lst[0] = lst[0]*10
-#     print(lst[0])
-#     print(lst[1])
-
-
-
-
-# lst[1] = (10*lst[0])+50
-
-
-# ans = []
-
-# for lst[0] in lst:
-#     x = fn(lst[0])
-#     ans.append(x)
-# print(ans)
-
-
 
 import json
  
@@ -81,24 +42,57 @@ with open('data.json') as json_file:
         totalProfit = retSP - manufacturingCost
         print("Total Profit : ", totalProfit)
 
-#STRATERGIES IMPLEMENTATION
-        num_str = repr(Sprice)
-        last_digit_str = num_str[-1]
-        last_digit = int(last_digit_str)
-        if last_digit_str == "8":
-            print( Sprice+1)
-        elif last_digit_str == "7":
-            print(Sprice+2)
-        elif last_digit_str == "6":
-            print(Sprice+3)
-        elif last_digit_str == "5":
-            print(Sprice+4)
-        elif last_digit_str == "4":
-            print(Sprice+5)
-        elif last_digit_str == "3":
-            print(Sprice+6)
-        elif last_digit_str == "2":
-            print(Sprice+7)
-        elif last_digit_str == "1":
-            print(Sprice+8)
+print("--------------------------------------------------------")
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # import pandas as pd
+
+# # def retailPricing(imp, *args):
+# #     inv=100
+# #     sellthrough=20
+# #     cost=10
+# #     profitpercentage=2
+# #     perishability=0
+# #     OPEX=25
+# #     print(OPEX + cost)
+# #     print(inv+sellthrough+cost+profitpercentage+perishability+OPEX)
+# # retailPricing(inv,sellthrough,cost,profitpercentage,perishability,OPEX)
+
+
+
+# def fn(a):
+#     return 10 * a
+
+# lst = [1, 2, 3, 4, 5, 6]
+
+# for i in lst:
+#     lst[0] = lst[0]*10
+#     print(lst[0])
+#     print(lst[1])
+
+
+
+
+# lst[1] = (10*lst[0])+50
+
+
+# ans = []
+
+# for lst[0] in lst:
+#     x = fn(lst[0])
+#     ans.append(x)
+# print(ans)
