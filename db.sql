b535c4409f149a63dc749d25f33477a178284fee
Vasista <vasista.tummala@gmail.com>
2022-06-25 23:06:36 +0530

    Update db.sql

--- db.sql
+++ db.sql
@@ -3,4 +3,4 @@
 SELECT * FROM prices;
 
 
-SELECT JSON_ARRAYAGG(JSON_OBJECT('inventory', inventory, 'OPEX', OPEX, 'sellthrough', sellthrough, 'cost', cost, 'profitpercentage', profitpercentage , 'perishability', perishability)) from prices;
+SELECT JSON_ARRAYAGG(JSON_OBJECT('inventory', inventory, 'OPEX', OPEX, 'sellthrough', sellthrough, 'cost', cost, 'profitpercentage', profitpercentage , 'perishability', perishability)) from prices;   
