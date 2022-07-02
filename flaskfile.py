995cb897dccc3029cea87eca26c07d561f6e2d1e
Vasista <vasista.tummala@gmail.com>
2022-07-02 14:37:27 +0530

    Update flaskfile.py

--- flaskfile.py
+++ flaskfile.py
@@ -14,11 +14,11 @@ def index():
     cost1 = str(pricewithprofit)
     Sprice1 = int(Sprice)
     a1 = int(a)
-    return flask.render_template("index.html", a1=a1, pricewithprofit=pricewithprofit)
-    # print("Price : ", Cost, "Discount : ", a )
-    return str(cost)
-    return str(a)
-    return "Hi"
+    return flask.render_template("index.html", a1=a1,Sprice=Sprice, pricewithprofit=pricewithprofit)
+    # # print("Price : ", Cost, "Discount : ", a )
+    # return str(cost)
+    # return str(a)
+    # return "Hi"
 # , Sprice=Sprice, a=a, cost=cost
 if __name__=="__main__":
     app.debug = True
