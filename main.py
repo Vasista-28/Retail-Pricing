25f7d112af5c6067b67cacf6a384f81276982817
Vasista <vasista.tummala@gmail.com>
2022-07-02 14:37:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -71,40 +71,54 @@ print("---------------------------------------------------------------")
 
 
 
-# # import pandas as pd
+# import json
+# import requests
+# import numpy as np
+# with open('data1.json') as json_file:
+#     data = json.load(json_file)
+#     # print(data)
+#     l = len(data)
+#     print("Data Length = ",l)
+#     for i in range(l):
+#         print("Data of ITEM ", i , " : ", data[i])
+    
+#     # print("\nPrinting nested dictionary as a key-value pair\n") 
+#     for i in data:
+#         inventory = i["inventory"]
+#         cost = i['cost']
+#         OPEX = i['OPEX']
+#         sellthrough = i['sellthrough']
+#         profitpercentage = i['profitpercentage']
+#         perishability = i['perishability']
+#         print("inventory = ", inventory)
+#         print("cost = ", cost)
+#         print("sellthrough = ", sellthrough)
+#         print("profitpercentage = ", profitpercentage)
+#         print("perishability = ", perishability)
+#         print("OPEX = ", OPEX)
+#         print("---------------------------------------------------------------")
 
-# # def retailPricing(imp, *args):
-# #     inv=100
-# #     sellthrough=20
-# #     cost=10
-# #     profitpercentage=2
-# #     perishability=0
-# #     OPEX=25
-# #     print(OPEX + cost)
-# #     print(inv+sellthrough+cost+profitpercentage+perishability+OPEX)
-# # retailPricing(inv,sellthrough,cost,profitpercentage,perishability,OPEX)
+#         pricewithprofit = (cost+OPEX) + ( ( profitpercentage * cost ) / 100 )
+#         quantity = inventory - sellthrough
+#         spoilage =  quantity * (perishability/100)
+#         SPriceofPerishable = pricewithprofit / (quantity - spoilage)
+
+#         print("Price With profit : ", pricewithprofit)
 
 
+#         Sprice = (pricewithprofit - SPriceofPerishable)
+#         intSprice = int(Sprice)
+#         print("Sprice : ", Sprice)
+#         print("int Sprice : ", intSprice)
 
-# def fn(a):
-#     return 10 * a
-
-# lst = [1, 2, 3, 4, 5, 6]
-
-# for i in lst:
-#     lst[0] = lst[0]*10
-#     print(lst[0])
-#     print(lst[1])
-
-
-
-
-# lst[1] = (10*lst[0])+50
-
-
-# ans = []
-
-# for lst[0] in lst:
-#     x = fn(lst[0])
-#     ans.append(x)
-# print(ans)
+#         manufacturingCost = inventory * cost
+#         print("Manufacturing : ", manufacturingCost )
+#         retSP = inventory * Sprice
+#         print("Selling : ", retSP)
+#         totalProfit = retSP - manufacturingCost
+#         print("Total Profit : ", totalProfit)
+#         print("Quantity = ", quantity)
+#         print("Spoilage = ", spoilage)
+#         print("Removed Perishable Price = ", SPriceofPerishable)
+#         print("---------------------------------------------------------------")
+#         print("---------------------------------------------------------------")
