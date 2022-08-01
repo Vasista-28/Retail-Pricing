958c129353a3b3da1b31d32d9b6bcd01e22a2df9
Vasista <vasista.tummala@gmail.com>
2022-08-01 22:54:39 +0530

    Update FINAL_MAIN.py

--- FINAL_MAIN.py
+++ FINAL_MAIN.py
@@ -1,6 +1,6 @@
 import json
 import requests
-import numpy as np
+# import numpy as np
 import webbrowser
 import time
 
@@ -25,61 +25,95 @@ with open('data1.json') as json_file:
         sellthrough = i['sellthrough']
         profitpercentage = i['profitpercentage']
         perishability = i['perishability']
-        print("inventory = ", inventory)
-        print("cost = ", cost)
-        print("sellthrough = ", sellthrough)
-        print("profitpercentage = ", profitpercentage)
+        print("inventory = ", inventory, "Units")
+        print("cost = ", cost, "Rupees")
+        print("sellthrough = ", sellthrough, "Units" )
+        print("profitpercentage = ", profitpercentage, "%")
         print("perishability = ", perishability)
-        print("OPEX = ", OPEX)
+        print("OpEx = ", OPEX, "Rupees")
         print("---------------------------------------------------------------")
 
-        pricewithprofit = (cost+OPEX) + ( ( profitpercentage * cost ) / 100 )
+        pricewithprofit = (cost+OPEX) + ( ( profitpercentage * (cost+OPEX) ) / 100 )
         quantity = inventory - sellthrough
         spoilage =  quantity * (perishability/100)
         SPriceofPerishable = pricewithprofit / (quantity - spoilage)
 
-        print("Price With profit : ", pricewithprofit)
+        print("MSRP : ", pricewithprofit, "Rupees")
 
 
         Sprice = (pricewithprofit - SPriceofPerishable)
         intSprice = int(Sprice)
-        print("Sprice : ", Sprice)
-        print("int Sprice : ", intSprice)
+
+        print("Selling price : ", round(Sprice,2), "Rupees")
 
         manufacturingCost = inventory * cost
-        print("Manufacturing : ", manufacturingCost )
+        # print("Total Manufacturing Cost : ", manufacturingCost )
         retSP = inventory * Sprice
-        print("Selling : ", retSP)
+        # 
         totalProfit = retSP - manufacturingCost
-        print("Total Profit : ", totalProfit)
-        print("Quantity = ", quantity)
-        print("Spoilage = ", spoilage)
-        print("Removed Perishable Price = ", SPriceofPerishable)
+        print("Quantity = ", round(quantity,2), "Units")
+        print("Spoilage = ", round(spoilage,2) )
+        print("Removed Perishable Price = ", round(SPriceofPerishable,2) , "Rupees")
         print("---------------------------------------------------------------")
-        print("---------------------------------------------------------------")
-        print("Sprice : ", Sprice)
-        print("int Sprice: ", intSprice)
 
+        print("Selling price : ", round(Sprice,2))
+        # print("int Sprice: ", intSprice)
+
+        print("---------------------------------------------------------------")
+        # print("int Sprice : ", intSprice)
+        print("Total Profit : ", round(totalProfit,2), "Rupees")
+        print("Total Selling Price : ", round(retSP,2), "Rupees")
 
     #STRATERGIES IMPLEMENTATION
 
         num_str = repr(intSprice)
         last_digit_str = num_str[-1]
         last_digit = int(last_digit_str)
-        if Sprice > 500:
-            x = 9 - int(last_digit_str)
-            global a
-            a = Sprice + x
-            print("Final Price : ", int(a))
+        # if Sprice > 500:
+        #     x = 9 - int(last_digit_str)
+        #     global a
+        #     a = Sprice + x
+        #     print("Final Price : ", int(a))
 
             
-        else:
-            print("Final Price : ", int(Sprice))
-
+        # else:
+        #     print("Final Price : ", int(Sprice))
+        if int(last_digit_str) == 1:
+            a1 = round(Sprice-2,2)
+            print("Final Price : ", a1, "Rupees")
+            # print("Final Price: ", round(Sprice - 2,2), "Rupees")
+        elif int(last_digit_str) == 2:
+            a1 = round(Sprice - 3,2)
+            print("Final Price: ", round(Sprice - 3,2), "Rupees")
+        elif int(last_digit_str) == 3:
+            a1 = round(Sprice - 4,2)
+            print("Final Price: ", round(Sprice - 4,2), "Rupees")
+        elif int(last_digit_str) == 4:
+            a1 = round(Sprice - 5,2)
+            print("Final Price: ", round(Sprice - 5,2), "Rupees")
+        elif int(last_digit_str) == 5:
+            a1 = round(Sprice - 6,2)
+            print("Final Price: ", round(Sprice - 6,2), "Rupees")
+        elif int(last_digit_str) == 6:
+            a1 = round(Sprice - 7,2)
+            print("Final Price: ", round(Sprice - 7,2), "Rupees")
+        elif int(last_digit_str) == 7:
+            a1 = round(Sprice - 8,2)
+            print("Final Price: ", round(Sprice - 8,2), "Rupees")
+        elif int(last_digit_str) == 8:
+            a1 = round(Sprice - 9,2)
+            print("Final Price: ", round(Sprice - 9,2), "Rupees")
+        elif int(last_digit_str) == 0:
+            a1 = round(Sprice - 1,2)
+            print("Final Price: ", round(Sprice - 1,2))
+        elif int(last_digit_str) == 9:
+            a1 = round(Sprice,2)
+            print("Final Price: ", round(Sprice,2))
+        
             # print("a = ", a)
             # x = ((a / pricewithprofit)*100)
             # discountpercent = 100-x
             # print(" Discount percentage : ", discountpercent)
         print("---------------------------------------------------------------")
         print("---------------------------------------------------------------")
-        print("---------------------------------------------------------------")
+        # print("---------------------------------------------------------------")
